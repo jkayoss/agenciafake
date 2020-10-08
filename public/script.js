637f34ed8d712b63142a858e552a533b153e58c6
jkayoss <51717883+jkayoss@users.noreply.github.com>
2020-10-08 13:37:45 -0300

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,14 +1,36 @@
 var myNav = document.querySelector('.scroll');
-window.onscroll = function () { 
-    var content = document.querySelector('.content')
+var myNav2 = document.querySelector('.scroll2');
+var content = document.querySelector('.content');
+    
+window.onscroll = function () {    
     if (window.pageYOffset > 20) {
-        myNav.classList.add("nav-colored");
+        
         content.style.height = '80px';
-        myNav.style.height = '80px';
+
+        if (myNav != null) {
+            myNav.classList.add("nav-colored");
+            myNav.style.height = '80px';
+        } 
+
+        if (myNav2 != null) {
+            myNav2.classList.add("nav-colored");
+            myNav2.style.height = '80px';
+        }  
     } 
     else {
-        myNav.classList.remove("nav-colored");
+
         content.style.height = '120px';
-        myNav.style.height = '120px';
+
+        if (myNav != null) {
+            myNav.classList.remove("nav-colored");
+            myNav.style.height = '120px';
+        } 
+
+        if (myNav2 != null) {
+            myNav2.classList.remove("nav-colored");
+            myNav2.style.height = '120px';
+        }  
     }
 };
+
+
